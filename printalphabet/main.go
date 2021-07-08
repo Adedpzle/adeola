aa335d893864020513f91182354fc1d9169ccf8b
Adeola <lyon100@yahoo.com>
2021-07-08 22:49:14 +0300

    whatever

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,11 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	i := 97
+	i := 122
 
-	for i <= 122 {
+	for i >= 97 {
 		z01.PrintRune(rune(i))
-		i++
+		i--
 	}
 
 	z01.PrintRune('\n')
