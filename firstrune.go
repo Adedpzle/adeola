42f0916a90be5221b3df4f397d62575d02e35a30
Adeola <lyon100@yahoo.com>
2021-07-15 11:37:09 +0300

    firstrune.go

--- firstrune.go
+++ firstrune.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"github.com/01-edu/z01"
-
 	"piscine"
 )
 
-func main() {
-	sentence := "Hello"
-	sentence := "Salut"
-	sentence := "Ola"
-
+func main( str string) string {
+		return strings.SplitN(str, "",2)[0]
+	}
 	z01.PrintRune(piscine.FirstRune("Hello!"))
 	z01.PrintRune(piscine.FirstRune("Salut!"))
 	z01.PrintRune(piscine.FirstRune("Ola!"))
