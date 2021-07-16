2065bfda6e03366b8a44b9adfcbdcfafa19490ff
Adeola <lyon100@yahoo.com>
2021-07-16 14:53:26 +0300

    appendrange.go

--- appendrange.go
+++ appendrange.go
@@ -1,6 +1,6 @@
 package piscine
 
-func Appendrange(value_0, value_1 int) int {
+func Min(value_0, value_1 int) int {
 	if value_0 < value_1 {
 		return value_0
 	}
@@ -8,7 +8,7 @@ func Appendrange(value_0, value_1 int) int {
 }
 
 // For 1+ values
-func Appendrange(value int, values ...int) int {
+func Mins(value int, values ...int) int {
 	for _, v := range values {
 		if v < value {
 			value = v
